b3b25f72db5eba20ae32a11ffbd40457681c6afb
umamumam <miftahulumam862@gmail.com>
2025-12-13 11:25:33 +0700

    Update Menu

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -56,6 +56,12 @@
                         <span class="pc-mtext">Hasil</span>
                     </a>
                 </li>
+                <li class="pc-item">
+                    <a href="/monthly-finances" class="pc-link">
+                        <span class="pc-micon"><i class="ti ti-report-analytics"></i></span>
+                        <span class="pc-mtext">Summary & Output</span>
+                    </a>
+                </li>
                 {{-- <li class="pc-item pc-hasmenu">
                     <a href="#!" class="pc-link"><span class="pc-micon"><i class="ti ti-chart-bar"></i>
                     </span><span class="pc-mtext">Hasil</span><span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
@@ -64,14 +70,14 @@
                         <li class="pc-item"><a class="pc-link" href="/incomes/detailhasil">Detail Hasil</a></li>
                     </ul>
                 </li> --}}
-                <li class="pc-item pc-hasmenu">
+                {{-- <li class="pc-item pc-hasmenu">
                     <a href="#!" class="pc-link"><span class="pc-micon"><i class="ti ti-report-analytics"></i>
                     </span><span class="pc-mtext">Summary & Output</span><span class="pc-arrow"><i data-feather="chevron-right"></i></span></a>
                     <ul class="pc-submenu">
                         <li class="pc-item"><a class="pc-link" href="/monthly-summaries">Monthly Summary</a></li>
                         <li class="pc-item"><a class="pc-link" href="/monthly-finances">Summary & Output</a></li>
                     </ul>
-                </li>
+                </li> --}}
                 <li class="pc-item pc-caption">
                     <label style="color: red;">Pengembalian / Pembatalan</label>
                     <i class="ti ti-news"></i>
